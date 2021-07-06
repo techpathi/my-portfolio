85e92837b2a76e5a296d3df2180a94a59a1f3d43
Venkatapathi Raju Gangiri <venkat4coding@gmail.com>
2021-07-06 21:38:43 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# my-portfolio
+ This is my portfolio https://techpathi.dev
---
+++ src/data.js
@@ -0,0 +1,39 @@
+export const projects = [
+  {
+    title: "CodeSlash",
+    subtitle: "ReactJs",
+    description:
+      "A HTML, CSS and JS code editor with web preview feature built using ReactJS and Ace Editor package.",
+    image: "./project1.png",
+    link: "https://codeslash.techpathi.dev",
+  },
+  {
+    title: "VaccSearch",
+    subtitle: "ReactJs",
+    description:
+      "A React application using the public COWIN APIs that facilitate users to search for COVID-19 vaccine slots.",
+    image: "./project2.png",
+    link: "https://vaccsearch.techpathi.dev",
+  },
+  {
+    title: "Voice of Vignan",
+    subtitle: "Android and Firebase",
+    description:
+      "Voice of Vignan is an Android application built on top of Firebase key features like authentication,real-time database,storage as well as cloud messaging. Cloud functions are also used to trigger instant notifications.",
+    image: "./project3.png",
+    link: "https://play.google.com/store/apps/details?id=com.dscviit.vov",
+  },
+]
+
+export const skills = [
+  "JavaScript",
+  "ReactJs",
+  "Node/Express.js",
+  "MongoDB",
+  "MySQL",
+  "HTML",
+  "CSS",
+  "Bootstrap",
+  "Android",
+  "Git",
+]
