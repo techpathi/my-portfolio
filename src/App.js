ed5c752245da40d2c996b6ea0227ec3051623a49
techpathi <venkat4coding@gmail.com>
2021-09-12 19:57:02 +0530

    Update projects & add social links

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
-import React from "react"
-import About from "./components/About"
-import Navbar from "./components/Navbar"
-import Projects from "./components/Projects"
-import Contact from "./components/Contact"
-import Skills from "./components/Skills"
+import React from 'react';
+import About from './components/About';
+import Navbar from './components/Navbar';
+import Projects from './components/Projects';
+import Contact from './components/Contact';
+import Skills from './components/Skills';
 
 export default function App() {
   return (
     <main>
-      <Navbar className="text-gray-400 bg-gray-900 body-font" />
+      <Navbar className='text-gray-400 bg-gray-900 body-font' />
       <About />
       <Projects />
       <Skills />
       <Contact />
     </main>
-  )
+  );
 }
--- src/data.js
+++ src/data.js
@@ -1,39 +1,47 @@
 export const projects = [
   {
-    title: "CodeSlash",
-    subtitle: "ReactJs",
+    title: 'CodeSlash',
+    subtitle: 'ReactJs',
     description:
-      "A HTML, CSS and JS code editor with web preview feature built using ReactJS and Ace Editor package.",
-    image: "./project1.png",
-    link: "https://codeslash.techpathi.dev",
+      'A HTML, CSS and JS code editor with web preview feature built using ReactJS and Ace Editor package.',
+    image: './project1.png',
+    link: 'https://codeslash.techpathi.dev'
   },
   {
-    title: "VaccSearch",
-    subtitle: "ReactJs",
+    title: 'SpaceX Watch',
+    subtitle: 'ReactJs',
     description:
-      "A React application using the public COWIN APIs that facilitate users to search for COVID-19 vaccine slots.",
-    image: "./project2.png",
-    link: "https://vaccsearch.techpathi.dev",
+      'A realtime SpaceX launch listing directory with live data from SpaceX APIs (docs.spacexdata.com/) ',
+    image: './project4.png',
+    link: 'https://spacexwatch.techpathi.dev'
   },
   {
-    title: "Voice of Vignan",
-    subtitle: "Android and Firebase",
+    title: 'VaccSearch',
+    subtitle: 'ReactJs',
     description:
-      "Voice of Vignan is an Android application built on top of Firebase key features like authentication,real-time database,storage as well as cloud messaging. Cloud functions are also used to trigger instant notifications.",
-    image: "./project3.png",
-    link: "https://play.google.com/store/apps/details?id=com.dscviit.vov",
+      'A React application using the public COWIN APIs that facilitate users to search for COVID-19 vaccine slots.',
+    image: './project2.png',
+    link: 'https://vaccsearch.techpathi.dev'
   },
-]
+  {
+    title: 'Voice of Vignan',
+    subtitle: 'Android and Firebase',
+    description:
+      'Voice of Vignan is an Android application to deliver instant updates for my undergrad college.',
+    image: './project3.png',
+    link: 'https://play.google.com/store/apps/details?id=com.dscviit.vov'
+  }
+];
 
 export const skills = [
-  "JavaScript",
-  "ReactJs",
-  "Node/Express.js",
-  "MongoDB",
-  "MySQL",
-  "HTML",
-  "CSS",
-  "Bootstrap",
-  "Android",
-  "Git",
-]
+  'JavaScript',
+  'ReactJs',
+  'Node/Express.js',
+  'MongoDB',
+  'HTML',
+  'CSS',
+  'Bootstrap',
+  'Ant Design for React',
+  'Android',
+  'Git'
+];
